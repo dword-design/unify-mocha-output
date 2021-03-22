8df217e0b730a2db96f8784d70c33cc2a04f360b
dword <info@dword-design.de>
2021-03-22 22:57:42 +0000

    fix: remove test times

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import { replace } from '@dword-design/functions'
 
-export default x =>
-  x |> replace(/(\d+) passing( \(\d+ms\))?/, '$1 passing') |> replace(/√/g, '✓')
+export default x => x |> replace(/ \(\d+ms\)/g, '') |> replace(/√/g, '✓')
